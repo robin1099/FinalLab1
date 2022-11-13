8e179ba642faf1be6a9b60f2f526a00966c0b0ff
robin1099 <106136964+robin1099@users.noreply.github.com>
2022-11-13 13:54:24 +0600

    Add files via upload

---
+++ ConversionRate.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body>
+        <center>
+        <h1>Page2[Conversion]</h1><br></br>
+</center>
+
+<h3>Conversion Rate</h3><br>
+1.<a href="home.php"> <b>  Home </b></a> 
+2.<a href="Conversion.php"> <b>  Conversion Rate </b></a> 
+3.<a href="history.php"><b> History </b></a><br><br>
+
+ <label for="uname"><b>Category:</b></label>
+      <input type="text" placeholder="" name="uname" required>
+	  <br><br>
+<label for="uname"><b>Unit:</b></label>
+      <input type="text" placeholder="" name="uname" required>
+	  <br><br>
+<label for="uname"><b>Rate:</b></label>
+      <input type="text" placeholder="" name="uname" required>
+	  <br><br>
+	  	<button type="submit">submit</button> 
+		</html>
---
+++ home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body>
+        <center>
+			<h1>Page1[Home]</h1><br></br>
+		</center>
+			<h3>Conversion Site</h3><br>
+			1.<a href="home.php"> <b> Home </b></a> 
+			2.<a href="ConversionRate.php"> <b>  Conversion Rate </b></a>
+			3.<a href="History.php"><b> History </b></a><br>
+		
+			<h3>Converter :</h3><br>
+			
+			Select Category: <select name="Category" id="Category">
+			
+			<option value="Usd to Bdt">Usd to Bdt</option>
+			<option value="Bdt to Usd">Bdt to Usd</option>	<br>
+            
+			Value: <select name="Catagory" id="Catagory">
+			<button type="submit" name="value" class="btn btn-primary form-control" style="width:30%;">Convert</button>
+			
+			<br><br>			
+			
+
+
+        
+        
+</html>
